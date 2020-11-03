a52565571ff3e2f2e4b6aae72b7b3ef7f4b23aeb
Shreyansh Bhagat <69347850+ShreyanshBhagat11@users.noreply.github.com>
2020-11-03 11:08:15 +0530

    Update Pass_Generator.java

--- Pass_Generator.java
+++ Pass_Generator.java
@@ -1,13 +1,11 @@
 public class Pass_Generator {
-    String PassGenerate(int len) {
-//        System.out.println("Generating password using random() : ");
-//        System.out.print("Your new password is : ");
+    String PassGenerate(int len) 
+    {
         String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String Small_chars = "abcdefghijklmnopqrstuvwxyz";
         String numbers = "0123456789";
         String symbols = "!@#$%^&*_=+-/.?<>)";
-//        String values = Capital_chars + Small_chars + numbers + symbols;
-//        Random rndm_method = new Random();
+
         String password = "";
         int j = 0;
         for (int i = 0; i < len; i++)
